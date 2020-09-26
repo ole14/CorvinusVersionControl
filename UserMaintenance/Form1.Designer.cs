ef9942ccecdd54d36ef9e634b1f3c347fe8d9642
ole14 <46139444+ole14@users.noreply.github.com>
2020-09-26 13:27:23 +0200

    5. feladat kész

--- UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/Form1.Designer.cs
@@ -28,13 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lbLastName = new System.Windows.Forms.Label();
+            this.lbFirstName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(242, 212);
+            this.listBox1.TabIndex = 0;
+            // 
+            // lbLastName
+            // 
+            this.lbLastName.AutoSize = true;
+            this.lbLastName.Location = new System.Drawing.Point(261, 16);
+            this.lbLastName.Name = "lbLastName";
+            this.lbLastName.Size = new System.Drawing.Size(35, 13);
+            this.lbLastName.TabIndex = 1;
+            this.lbLastName.Text = "label1";
+            // 
+            // lbFirstName
+            // 
+            this.lbFirstName.AutoSize = true;
+            this.lbFirstName.Location = new System.Drawing.Point(261, 42);
+            this.lbFirstName.Name = "lbFirstName";
+            this.lbFirstName.Size = new System.Drawing.Size(35, 13);
+            this.lbFirstName.TabIndex = 2;
+            this.lbFirstName.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(306, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(306, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(262, 68);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(188, 23);
+            this.btAdd.TabIndex = 5;
+            this.btAdd.Text = "button1";
+            this.btAdd.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(469, 239);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbFirstName);
+            this.Controls.Add(this.lbLastName);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lbLastName;
+        private System.Windows.Forms.Label lbFirstName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btAdd;
     }
 }
 
--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Properties;
 
 namespace UserMaintenance
 {
@@ -15,6 +16,9 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            lbLastName.Text = Resource1.LastName;
+            lbFirstName.Text = Resource1.FirstName;
+            btAdd.Text = Resource1.Add;
         }
     }
 }
