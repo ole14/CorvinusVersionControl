b4c37ef789ea40a083a7d980f4b9839ed7ac08fd
ole14 <46139444+ole14@users.noreply.github.com>
2020-09-26 14:11:26 +0200

    Csak teljes névvel

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -7,18 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 using UserMaintenance.Properties;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
-            lbLastName.Text = Resource1.LastName;
-            lbFirstName.Text = Resource1.FirstName;
+            lbFullName.Text = Resource1.FullName;
             btAdd.Text = Resource1.Add;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                FullName = textFullName.Text
+            };
+            users.Add(u);
         }
     }
 }
