8c44fd6cf1d8304c5233d5ecbac83239d48da829
ole14 <46139444+ole14@users.noreply.github.com>
2020-09-30 23:17:37 +0200

    CreateExcel megírva

--- ExcelExport/Form1.cs
+++ ExcelExport/Form1.cs
@@ -16,12 +16,49 @@ namespace ExcelExport
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+
+        Excel.Application x1App; // Alkalmazás változó
+        Excel.Workbook x1WB; //Munkafüzet
+        Excel.Worksheet x1Sheet; //Munkalap neve
         
         public Form1()
         {
             InitializeComponent();
             LoadData();
-            
+            CreateExcel();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                x1App = new Excel.Application(); //ez indítja az excelt és tölti be az objektumot
+                x1WB = x1App.Workbooks.Add(Missing.Value); //Új munkafüzet
+                x1Sheet = x1WB.ActiveSheet(); //Új munkalap
+
+                // CreateTable();
+
+                x1App.Visible = true;
+                x1App.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                //hibára futva excel bezárása
+                x1WB.Close(false, Type.Missing, Type.Missing);
+                x1App.Quit();
+                x1WB = null;
+                x1App = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+            throw new NotImplementedException();
         }
 
         private void LoadData() 
