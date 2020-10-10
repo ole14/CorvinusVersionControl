9192b339bf9883ccd5ae7d3bea11c65233f85f40
ole14 <46139444+ole14@users.noreply.github.com>
2020-10-10 14:46:53 +0200

    7. feladat bemásolva

--- VaR/Form1.cs
+++ VaR/Form1.cs
@@ -25,6 +25,11 @@ namespace VaR
             CreatePortfolio();
         }
 
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
         private void CreatePortfolio()
         {
             Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
@@ -32,6 +37,21 @@ namespace VaR
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
 
             dataGridView2.DataSource = Portfolio;
+        }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
