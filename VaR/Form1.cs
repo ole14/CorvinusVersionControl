9a181722824ca0ce02cc1b8147ce8d0d3c0a00da
ole14 <46139444+ole14@users.noreply.github.com>
2020-10-15 19:37:57 +0200

    2 óra szenvedés és csoporttárs segítségével sikerült megoldani a feladatot, ebből a leckéből semmit nem tanultam, mert nem volt elégséges a magyarázat

--- VaR/Form1.cs
+++ VaR/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +24,50 @@ namespace VaR
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
-        }
 
-        private decimal GetPortfolioValue(DateTime date)
-        {
-            throw new NotImplementedException();
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+            
+           SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Pontosvesszővel tagolt szöveg (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                sw.Write("Időszak");
+                sw.Write(";");
+                sw.Write("Nyereség");
+                sw.WriteLine();
+                int y = 1;
+                foreach (var nyer in nyereségekRendezve)
+                {
+                    sw.Write(y);
+                    sw.Write(";");
+                    sw.Write(nyer.ToString());
+                    sw.Write(";");
+                    sw.WriteLine();
+                    y++;
+                }
+            }
         }
 
         private void CreatePortfolio()
