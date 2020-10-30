76073d6778a06260755ba73889506a28a6601e4b
ole14 <ole014@gmail.com>
2020-10-30 19:52:55 +0100

    3. feladat

---
+++ MicroSimulation/MicroSimulation/Entities/Gender.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroSimulation.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Felmale = 2
+    }
+
+    public class Person
+    {
+        public int BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double Probability { get; set; }
+    }
+
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double Probability { get; set; }
+    }
+}
