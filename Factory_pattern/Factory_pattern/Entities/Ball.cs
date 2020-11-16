aa08041a0ad3e2a5511f35bfed347f838552d390
ole14 <ole014@gmail.com>
2020-11-16 19:49:59 +0100

    2. feladat kész

---
+++ Factory_pattern/Factory_pattern/Entities/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factory_pattern.Entities
+{
+    class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = 50;
+            Paint += BallPaint;
+        }
+
+        private void BallPaint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics graphics)
+        {
+            graphics.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall() 
+        {
+            Left += 1;
+        }
+    }
+}
