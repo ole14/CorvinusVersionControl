0c5e91623d06e2448cd9e8ce2d598963a05ecb79
ole14 <ole014@gmail.com>
2020-11-26 18:55:23 +0100

    4. feladat

--- worldHardestG/worldHardestG/Form1.cs
+++ worldHardestG/worldHardestG/Form1.cs
@@ -16,12 +16,27 @@ namespace worldHardestG
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 100;
+        int generation = 1;
+
+
         public Form1()
         {
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+                gc.Start();
+            }
+            
+            /*gc.AddPlayer();
+            gc.Start(true);
+            */
         }
     }
 }
